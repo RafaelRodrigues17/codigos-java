844a6d16dec70218307b47c41e42db217c8615e0
RafaelRodrigues17 <rrodriguesmattos27@gmail.com>
2024-10-10 14:09:47 -0300

    Update Jogo.java

--- Pedra Papel Tesoura/Jogo.java
+++ Pedra Papel Tesoura/Jogo.java
@@ -1,36 +1,39 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class Jogo {
+    public class Jogo{
     private static final String[] opcoes = {"Pedra", "Papel", "Tesoura"};
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
-
-        System.out.println("Escolha uma opção: Pedra, Papel ou Tesoura:");
+        System.out.println("Escolha: Pedra, Papel ou Tesoura:");
         String escolhaUsuario = scanner.nextLine().trim();
 
         int escolhaComputadorIndex = random.nextInt(3);
         String escolhaComputador = opcoes[escolhaComputadorIndex];
 
         System.out.println("Computador escolheu: " + escolhaComputador);
-        
-        String resultado = determinarVencedor(escolhaUsuario, escolhaComputador);
+        String resultado = escolhaVencedor(escolhaUsuario, escolhaComputador);
         System.out.println(resultado);
 
         scanner.close();
     }
 
-    public static String determinarVencedor(String escolhaUsuario, String escolhaComputador) {
-        if (escolhaUsuario.equalsIgnoreCase(escolhaComputador)) {
+    public static String escolhaVencedor(String escolhaUsuario, String escolhaComputador) {
+        if(escolhaUsuario.equalsIgnoreCase(escolhaComputador)) {
             return "Empate!";
-        } else if ((escolhaUsuario.equalsIgnoreCase("Pedra") && escolhaComputador.equals("Tesoura")) ||
+
+        }else if ((escolhaUsuario.equalsIgnoreCase("Pedra") && escolhaComputador.equals("Tesoura")) ||
                 (escolhaUsuario.equalsIgnoreCase("Papel") && escolhaComputador.equals("Pedra")) ||
                 (escolhaUsuario.equalsIgnoreCase("Tesoura") && escolhaComputador.equals("Papel"))) {
-            return "Você venceu!";
-        } else {
-            return "Você perdeu!";
+
+            return "Você Ganhou!";
+        }else{
+            return "Você Perdeu!";
         }
     }
 }
+
+
+
